4134ba6810ae215e39cd69ec732449f3f3bfef5f
Zhonghao Wang <wongzhonghao123@gmail.com>
2025-05-29 21:45:55 +0800

    update doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
 import os
 import sys
 from pathlib import Path
-# sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, str(Path('../../', 'utils').resolve()))
 # sys.path.insert(0, str(Path('../../', 'predictor').resolve()))
 
